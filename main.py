70711cf838e8f40f151970da51aed55b420e395f
Thibak <russinow.m@gmail.com>
2014-11-25 12:14:54 +0300

    getting datetime from server

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ Created on Fri Nov 21 14:07:31 2014
 import os
 from ftplib import FTP
 import time
-import datetime
+from datetime import datetime
 
-path = "D:\AC\DBMirror\ "
-logmsg = '\nStart session at '
+path = "D:\AC\DBMirror\\"
+logmsg = '\n----------------------\n Start session at '
 lastupdate = {}
 
 ts = time.time()
-logmsg += datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+logmsg += datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 logmsg += '\n'
 
 try:
@@ -31,13 +31,12 @@ try:
     listing = []
     ftp.retrlines("LIST", listing.append)
     words = [i.split(None, 8) for i in listing]
-    lastupdate =  {i[-1].lstrip():'_'.join(i[5:8]) for i in words}
+    lastupdate =  {i[-1].lstrip():datetime.strptime(ftp.sendcmd('MDTM ' + i[-1].lstrip())[4:], "%Y%m%d%H%M%S") for i in words}
     logmsg += 'Last updates:\n'
-    logmsg +=  ''.join([str(i)+' = '+str(lastupdate[i])+'\n' for i in lastupdate])
-    
+    logmsg +=  ''.join([str(i)+' = '+lastupdate[i].strftime("%d %B %Y %H:%M:%S")+'\n' for i in lastupdate])
     logmsg += 'Start update process\n'
     for bases in lastupdate:
-        d = path + str(bases) +'\ '+ lastupdate[bases]
+        d = path + str(bases) +'\\'+ lastupdate[bases].strftime("%d_%B_%Y")
         if not os.path.exists(d):
             os.makedirs(d)
             logmsg += str(bases)
@@ -48,11 +47,11 @@ try:
             w = [i.split(None, 8) for i in l]
             lfn = [i[-1].lstrip() for i in w]
             for i in lfn:
-                lf = open(str(d) + '\ '+str(i), "wb")
+                lf = open(str(d) + '\\'+str(i), "wb")
                 ftp.retrbinary("RETR " + str(i), lf.write)
                 lf.close()
             ftp.cwd('..')
-            logmsg += 'DB successful updated\n----------------------------------'
+            logmsg += 'DB successful updated\n----------------------------------\n'
         else:
             logmsg += str(bases)
             logmsg += ' -- NO UPDATES\n'
@@ -60,6 +59,7 @@ try:
     ftp.cwd('OL')
     
 except BaseException as a:
+    logmsg += 'Exception erised\n'
     logmsg += str(a.args)
   
   
